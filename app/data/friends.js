37550d6b4ce7293bc4787d193a1f92af8182f1f6
Mustafa Muhammad <m.muhammad43081@gmail.com>
2019-11-23 19:42:12 -0500

    setting number comparisons, getting photo to pop up on modal

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,61 +4,61 @@ var friendsArray = [
     {
         friendName: "Prince",
         photo: "https://ibb.co/yyM13R0",
-        scores: [1, 3, 7, 2, 2, 1, 3, 3, 4, 5],
+        surveyResponse: [1, 3, 7, 2, 2, 1, 3, 3, 4, 5],
         
     },
     {
         friendName: "Helen",
         photo: "https://ibb.co/hm37c6n",
-        scores: [3, 3, 6, 6, 2, 1, 4, 3, 3, 5],
+        surveyResponse: [3, 3, 6, 6, 2, 1, 4, 3, 3, 5],
         
     },
     {
         friendName: "Harriet",
         photo: "https://ibb.co/37pbB22",
-        scores: [4, 4, 4, 2, 5, 5, 3, 2, 4],
+        surveyResponse: [4, 4, 4, 2, 5, 5, 3, 2, 4],
         
     },
     {
         friendName: "Beaker",
         photo: "https://ibb.co/jkwj5jh",
-        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        surveyResponse: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
         
     },
     {
         friendName: "Thor",
         photo: "https://ibb.co/dKjnK1D",
-        scores: [3, 3, 5, 1, 1, 2, 2, 5, 4, 2],
+        surveyResponse: [3, 3, 5, 1, 1, 2, 2, 5, 4, 2],
         
     },
     {
         friendName: "James",
         photo: "https://ibb.co/Tq25CNT",
-        scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+        surveyResponse: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
         
     },
     {
         friendName: "Janelle",
         photo: "https://ibb.co/wZncp6H",
-        scores: [1, 2, 3, 4, 5, 5, 4, 3, 2, 1],
+        surveyResponse: [1, 2, 3, 4, 5, 5, 4, 3, 2, 1],
         
     },
     {
         friendName: "Eddie",
         photo: "https://ibb.co/MC2KpGT",
-        scores: [2, 4, 1, 3, 5, 5, 3, 1, 2, 1],
+        surveyResponse: [2, 4, 1, 3, 5, 5, 3, 1, 2, 1],
         
     },
     {
         friendName: "Tina",
         photo: "https://ibb.co/BKGgBjt",
-        scores: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
+        surveyResponse: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
         
     },
     {
         friendName: "Jimmy",
         photo: "https://ibb.co/hsbdTmR",
-        scores: [3, 4, 4, 4, 4, 4, 4, 4, 1, 4],
+        surveyResponse: [3, 4, 4, 4, 4, 4, 4, 4, 1, 1],
         
     },
 ];
--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -8,23 +8,25 @@ app.get("/api/friends", function(req, res) {
     res.json(friendsArray);
   });
 
-  app.post("/api/friends", function(req, res) {
-
-    if (friendsArray.length < 5) {
-      friendsArray.push(req.body);
-      res.json(true);
-    }
-    // else {
-    //   waitListData.push(req.body);
-    //   res.json(false);
-    // }
-  });
+  // app.post("/api/friends", function(req, res) {
+  //   //LOOP OVER FRIENDS ARRAY AND COMPARE RESPONSES
+  //   var addFriend = req.body;
+  //   var surveyScore = 0;
+  //   var friendScore = 0;
+  //   var friendMatch = {
+  //     friendName: "",
+  //       photo: "",
+  //       surveyResponse: 99999
+  //   }
+ 
+    
+    
+  // });
 
  
   app.post("/api/clear", function(req, res) {
     //REMOVE ARRAY DATA
     friendsArray.length = 0;
-    // waitListData.length = 0;
 
     res.json({ ok: true });
   });
