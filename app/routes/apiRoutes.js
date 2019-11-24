09b13bd85dcff3c4f706fe6749ac16938e1f3034
Mustafa Muhammad <m.muhammad43081@gmail.com>
2019-11-23 20:32:54 -0500

    Onclick not responding to submit.

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -1,16 +1,16 @@
 //DATA LOAD
-var friendsArray = require("../data/friends.js");
+var friendInfo = require("../data/friends.js");
 
 
 //MODULE FOR EXPORTS
 module.exports = function(app) {
 app.get("/api/friends", function(req, res) {
-    res.json(friendsArray);
+    res.json(friendInfo);
   });
 
   app.post("/api/clear", function(req, res) {
     //REMOVING ARRAY DATA
-    friendsArray.length = 0;
+    friendInfo.length = 0;
     res.json({ ok: true });
   });
 };
