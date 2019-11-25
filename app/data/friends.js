38c0bbce3046bb245d64aaf1d2208c6a66400d78
Mustafa Muhammad <m.muhammad43081@gmail.com>
2019-11-24 19:48:41 -0500

    reobserving and changint backend pieces

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,62 +4,171 @@ var friendsArray = [
     {
         friendName: "Prince",
         photo: "https://ibb.co/yyM13R0",
-        surveyResponse: [1, 3, 7, 2, 2, 1, 3, 3, 4, 5],
-        
+        surveyResponse: [
+            1, 
+            3, 
+            7, 
+            2, 
+            2, 
+            1, 
+            3, 
+            3, 
+            4, 
+            5,
+        ],
+
     },
     {
         friendName: "Helen",
         photo: "https://ibb.co/hm37c6n",
-        surveyResponse: [3, 3, 6, 6, 2, 1, 4, 3, 3, 5],
-        
+        surveyResponse: [
+            3, 
+            3, 
+            6, 
+            6, 
+            2, 
+            1,
+            4, 
+            3, 
+            3, 
+            5,
+        ],
+
     },
     {
         friendName: "Harriet",
         photo: "https://ibb.co/37pbB22",
-        surveyResponse: [4, 4, 4, 2, 5, 5, 3, 2, 4],
-        
+        surveyResponse: [
+            4, 
+            4, 
+            4, 
+            2, 
+            5, 
+            5, 
+            3, 
+            2, 
+            4,
+        ],
+
     },
     {
         friendName: "Beaker",
         photo: "https://ibb.co/jkwj5jh",
-        surveyResponse: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        
+        surveyResponse: [
+            1, 
+            1, 
+            1, 
+            1, 
+            1, 
+            1, 
+            1, 
+            1, 
+            1, 
+            1,
+        ],
+
     },
     {
         friendName: "Thor",
         photo: "https://ibb.co/dKjnK1D",
-        surveyResponse: [3, 3, 5, 1, 1, 2, 2, 5, 4, 2],
-        
+        surveyResponse: [
+            3, 
+            3, 
+            5, 
+            1, 
+            1, 
+            2, 
+            2, 
+            5, 
+            4, 
+            2,
+        ],
+
     },
     {
         friendName: "James",
         photo: "https://ibb.co/Tq25CNT",
-        surveyResponse: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
-        
+        surveyResponse: [
+            5, 
+            5, 
+            5, 
+            5, 
+            5, 
+            5, 
+            5, 
+            5, 
+            5, 
+            5,
+        ],
+
     },
     {
         friendName: "Janelle",
         photo: "https://ibb.co/wZncp6H",
-        surveyResponse: [1, 2, 3, 4, 5, 5, 4, 3, 2, 1],
-        
+        surveyResponse: [
+            1, 
+            2, 
+            3, 
+            4, 
+            5, 
+            5, 
+            4, 
+            3, 
+            2, 
+            1,
+        ],
+
     },
     {
         friendName: "Eddie",
         photo: "https://ibb.co/MC2KpGT",
-        surveyResponse: [2, 4, 1, 3, 5, 5, 3, 1, 2, 1],
-        
+        surveyResponse: [
+            2, 
+            4, 
+            1, 
+            3, 
+            5, 
+            5, 
+            3, 
+            1, 
+            2, 
+            1,
+        ],
+
     },
     {
         friendName: "Tina",
         photo: "https://ibb.co/BKGgBjt",
-        surveyResponse: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
-        
+        surveyResponse: [
+            4, 
+            4, 
+            4, 
+            4, 
+            4, 
+            4, 
+            4, 
+            4, 
+            4, 
+            4,
+        ],
+
     },
     {
         friendName: "Jimmy",
         photo: "https://ibb.co/hsbdTmR",
-        surveyResponse: [3, 4, 4, 4, 4, 4, 4, 4, 1, 1],
-        
+        surveyResponse: [
+            3, 
+            4, 
+            4, 
+            4, 
+            4, 
+            4, 
+            4, 
+            4, 
+            1, 
+            1,
+        ],
+
     },
 ];
 
